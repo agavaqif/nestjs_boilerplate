f84a1f98668e86582cb2ead267af1674cd233ae0
Vagif Aghayev <agavaqif@yahoo.com>
2021-03-28 21:57:08 -0400

    Fix

--- src/enums/error-codes.enum.ts
+++ src/enums/error-codes.enum.ts
@@ -1,11 +1,11 @@
-export declare enum ErrorCode {
+export  enum ErrorCode {
     NOT_FOUND = "notFound",
     NOT_UNIQUE = "notUnique",
     SERVER_ERROR = "serverError",
     NO_DATA = "noData"
 }
 
-export declare enum ErrorMessage {
+export  enum ErrorMessage {
     NOT_FOUND = "Resource was not found",
     NOT_UNIQUE = "is not unique",
     SERVER_ERROR = "Server Error",
